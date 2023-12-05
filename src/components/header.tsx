fdcaad87c3764477c48a7e013b73c4cb701283a9
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-05 15:39:54 +0000

    refactor: Header component to use destructuring and remove the interface as it's just one value being passed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,8 @@
-interface HeaderProps {
-  numCats: number;
-}
-
-const Header: React.FC<HeaderProps> = (props) => (
+const Header = ({ numCats }: { numCats: number }) => (
   <header className="header__container">
     <h1 className="header__title header__welcome">Welcome to React!</h1>
     <h2 className="header__title">
-      There are currently {props.numCats} Cats in this Cat App
+      There are currently {numCats} Cats in this Cat App
     </h2>
   </header>
 );
